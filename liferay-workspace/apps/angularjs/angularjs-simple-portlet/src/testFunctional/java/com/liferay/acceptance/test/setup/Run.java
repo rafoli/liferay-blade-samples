360e6a3e2558606fc09366540d9d4ca334374d0e
Rafael Oliveira <rafael.oliveira@liferay.com>
2017-09-14 11:23:59 -0300

    Use Cucumber Runner

--- liferay-workspace/apps/angularjs/angularjs-simple-portlet/src/testFunctional/java/com/liferay/acceptance/test/setup/Run.java
+++ liferay-workspace/apps/angularjs/angularjs-simple-portlet/src/testFunctional/java/com/liferay/acceptance/test/setup/Run.java
@@ -7,18 +7,19 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import com.liferay.gs.testFramework.UtilsKeys;
 
 /**
  * Created by Haysa on 07/06/17.
  */
-@RunAsClient
-@RunWith(Arquillian.class)
+@RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources"},
-        format = {"pretty", "html:reports/acceptanceTestReport"},
-        glue = {"com/liferay/acceptance/test/steps"})
+        features = "src/testFunctional/resources",
+        glue = {"com/liferay/acceptance/test/steps"},
+        plugin = {"pretty", "html:reports/cucumber"},
+        tags = {})
 public class Run {
 
-	@Drone
-	public static WebDriver webDriver;
+        public static WebDriver webDriver = UtilsKeys.DRIVER;
 }
